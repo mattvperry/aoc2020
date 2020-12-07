4e4d0a514e46a9e39be9b380f01ee6f259b6b11d
Matt Perry <muffinman616@gmail.com>
2020-12-07 13:20:36 -0500

    Clean af

--- day7/day7.ts
+++ day7/day7.ts
@@ -4,22 +4,19 @@ type BagColor = string;
 type ColorGraph = Record<BagColor, (readonly [BagColor, number])[]>;
 
 const parse = (line: string): Entries<ColorGraph> => {
-    const [first, ...rest] = line.split(',');
-    const [bag, contains] = first.split('contain');
-    const [keyColor1, keyColor2] = bag.split(' ');
-    const key = `${keyColor1} ${keyColor2}`;
-    const value = !contains.endsWith('no other bags.') ? [contains, ...rest].map(x => {
-        const [num, color1, color2] = x.trim().split(' ');
+    const [color, rest] = line.split(' bags contain ');
+    const value = rest !== 'no other bags.' ? rest.split(', ').map(x => {
+        const [num, color1, color2] = x.split(' ');
         return [`${color1} ${color2}`, parseInt(num, 10)] as const;
     }) : [];
-    return [key, value];
+
+    return [color, value];
 };
 
 const hasPath = memoize(
-    (graph: ColorGraph, from: BagColor, to: BagColor): boolean =>
-        from !== to
-            ? graph[from].some(([node]) => hasPath(graph, node, to))
-            : true,
+    (graph: ColorGraph, from: BagColor, to: BagColor): boolean => from !== to
+        ? graph[from].some(([color]) => hasPath(graph, color, to))
+        : true,
     (_, f, __) => f);
 
 const countBags = memoize(
@@ -27,18 +24,16 @@ const countBags = memoize(
         1 + graph[bag].reduce((acc, [color, num]) => acc + (num * countBags(graph, color)), 0),
     (_, c) => c);
 
-const target = 'shiny gold';
-
-const part1 = (graph: ColorGraph): number =>
-    countBy(Object.keys(graph), key => key !== target && hasPath(graph, key, target));
-
-const part2 = (graph: ColorGraph): number =>
-    countBags(graph, target) - 1;
+const day7 = (graph: ColorGraph): [number, number] => {
+    const target = 'shiny gold';
+    return [
+        countBy(Object.keys(graph), color => color !== target && hasPath(graph, color, target)),
+        countBags(graph, target) - 1,
+    ];
+};
 
 (async () => {
     const lines = await readInputLines('day7');
     const graph = fromEntries(lines.map(parse));
-
-    console.log(part1(graph));
-    console.log(part2(graph));
+    console.log(...day7(graph));
 })();
