e2540cd618e2421ad5f95a50da231b79097dc4d0
Matt Perry <muffinman616@gmail.com>
2020-12-11 11:37:14 -0500

    Cleanup

--- day11/day11.ts
+++ day11/day11.ts
@@ -1,7 +1,7 @@
 import { countBy, Entries, fromEntries, isDefined, readInputLines } from "../shared/utils";
 
 type Pos = readonly [number, number];
-type Adjacent = (pos: Pos, seats: Seats) => Iterable<Pos>;
+type Adjacent = (pos: Pos, seats: Seats, dx: number, dy: number) => Pos;
 type Seats = Partial<Record<string, boolean>>;
 type Room = {
     seats: Seats;
@@ -29,12 +29,24 @@ const fromStr = (str: string): Pos => {
 
 const toStr = (pos: Pos): string => pos.join(',');
 
+const dirs = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+    [1, 1],
+    [1, -1],
+    [-1, 1],
+    [-1, -1],
+] as const;
+
 function* changes(seats: Seats, rule: number, adj: Adjacent): Iterable<Entries<Seats>> {
     for (const pos of Object.keys(seats)) {
-        const num = countBy(adj(fromStr(pos), seats), x => seats[toStr(x)] ?? false);
-        if (seats[pos] && num >= rule) {
+        const neighboors = dirs.map(([dx, dy]) => adj(fromStr(pos), seats, dx, dy))
+        const occupied = countBy(neighboors, x => seats[toStr(x)] ?? false);
+        if (seats[pos] && occupied >= rule) {
             yield [pos, false];
-        } else if (!seats[pos] && num === 0) {
+        } else if (!seats[pos] && occupied === 0) {
             yield [pos, true];
         }
     }
@@ -53,34 +65,19 @@ const settle = (seats: Seats, rule: number, adj: Adjacent): number => {
     return countBy(Object.values(seats), x => x ?? false);
 };
 
-const dirs = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-    [1, 1],
-    [1, -1],
-    [-1, 1],
-    [-1, -1],
-] as const;
-
-const part1 = ({ seats }: Room): number => settle(seats, 4, function* ([x, y]) {
-    for (const [dx, dy] of dirs) {
-        yield [x + dx, y + dy];
-    }
+const part1 = ({ seats }: Room): number => settle(seats, 4, ([x, y], _, dx, dy) => {
+    return [x + dx, y + dy];
 });
 
-const part2 = ({ seats, width, height }: Room): number => settle(seats, 5, function* (pos, seats) {
-    for (const [dx, dy] of dirs) {
-        let [x, y] = pos;
-        while (x < width && x > -1 && y < height && y > -1) {
-            [x, y] = [x + dx, y + dy];
-            if (isDefined(seats[toStr([x, y])])) {
-                yield [x, y];
-                break;
-            }
+const part2 = ({ seats, width, height }: Room): number => settle(seats, 5, ([x, y], seats, dx, dy) => {
+    while (x < width && x > -1 && y < height && y > -1) {
+        [x, y] = [x + dx, y + dy];
+        if (isDefined(seats[toStr([x, y])])) {
+            return [x, y];
         }
     }
+
+    return [x, y];
 });
 
 (async () => {
