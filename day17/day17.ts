cd71299ae09795816001e25b7e0b88dc92717f41
Matt Perry <muffinman616@gmail.com>
2020-12-17 15:50:29 -0500

    n dims day17

--- day17/day17.ts
+++ day17/day17.ts
@@ -1,62 +1,59 @@
-import { countBy, map, readInputLines } from "../shared/utils";
+import { countBy, map, readInputLines, zipWith } from "../shared/utils";
 
-type Coord = [number, number, number, number];
-type CoordS = `${number}_${number}_${number}_${number}`;
+type Coord = number[];
+type CoordS = string;
 type Conway = Map<CoordS, boolean>;
 
-const parse = (lines: string[]): Conway => new Map([...(function* () {
+const parse = (lines: string[], dims: number): Conway => new Map([...(function* () {
     for (let y = 0; y < lines.length; ++y) {
         for (let x = 0; x < lines[0].length; ++x) {
             if (lines[y][x] == '#') {
-                yield [toStr([x, y, 0, 0]), true] as const;
+                yield [toStr([x, y, ...Array(dims - 2).fill(0)]), true] as const;
             }
         }
     }
 })()]);
 
-const toStr = ([x, y, z, w]: Coord): CoordS => `${x}_${y}_${z}_${w}` as const;
+const toStr = (coord: Coord): CoordS =>
+    coord.map(x => x.toString()).join('_');
 
 const fromStr = (coord: CoordS): Coord => coord
     .split('_')
     .map(x => parseInt(x, 10)) as Coord;
 
-function* neighboors(s: CoordS): Iterable<CoordS> {
-    const [x, y, z, w] = fromStr(s);
-    for (let dx = -1; dx <= 1; ++dx) {
-        for (let dy = -1; dy <= 1; ++dy) {
-            for (let dz = -1; dz <= 1; ++dz) {
-                for (let dw = -1; dw <= 1; ++dw) {
-                    if (dx === 0 && dy === 0 && dz === 0 && dw === 0) {
-                        continue;
-                    }
+const neighboors = (dims: number) => {
+    const deltas = Array((3 ** dims) - 1).fill([]).map((_, i) => (i + 1)
+        .toString(3)
+        .padStart(dims, '0')
+        .split('')
+        .map(d => d === '2' ? -1 : parseInt(d, 10)));
 
-                    yield toStr([x + dx, y + dy, z + dz, w + dw]);
-                }
-            }
-        }
-    }
-};
+    return (s: CoordS) =>
+        map(deltas, delta => toStr(zipWith(delta, fromStr(s), (x, y) => x + y)));
+}
 
-function* expand(conway: Conway): Iterable<readonly [CoordS, boolean]> {
+function* expand(conway: Conway, fns: (s: CoordS) => Iterable<CoordS>): Iterable<readonly [CoordS, boolean]> {
     for (const [coord, active] of conway) {
         yield [coord, active] as const;
-        yield* map(neighboors(coord), n => [n, conway.get(n) ?? false] as const);
+        yield* map(fns(coord), n => [n, conway.get(n) ?? false] as const);
     }
 }
 
-function* step(conway: Conway): Iterable<readonly [CoordS, boolean]> {
+function* step(conway: Conway, fns: (s: CoordS) => Iterable<CoordS>): Iterable<readonly [CoordS, boolean]> {
     for (const [c, active] of conway) {
-        const count = countBy(neighboors(c), x => conway.get(x) ?? false);
+        const count = countBy(fns(c), x => conway.get(x) ?? false);
         if (count === 3 || (active && count === 2)) {
             yield [c, true] as const;
         }
     }
 }
 
-const part2 = (conway: Conway): number => {
+const day17 = (lines: string[], dims: number): number => {
+    const fns = neighboors(dims);
+    let conway = parse(lines, dims);
     for (let i = 0; i < 6; ++i) {
-        conway = new Map(expand(conway));
-        conway = new Map(step(conway));
+        conway = new Map(expand(conway, fns));
+        conway = new Map(step(conway, fns));
     }
 
     return conway.size;
@@ -64,7 +61,7 @@ const part2 = (conway: Conway): number => {
 
 (async () => {
     const lines = await readInputLines('day17');
-    const conway = parse(lines);
 
-    console.log(part2(conway));
+    console.log(day17(lines, 3));
+    console.log(day17(lines, 4));
 })();
