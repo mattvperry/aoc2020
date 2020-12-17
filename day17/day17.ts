2095fd275439628fae40ff32fc437ae0ff998fd9
Matt Perry <muffinman616@gmail.com>
2020-12-17 04:12:34 -0500

    Clean

--- day17/day17.ts
+++ day17/day17.ts
@@ -2,13 +2,13 @@ import { countBy, map, readInputLines } from "../shared/utils";
 
 type Coord = [number, number, number, number];
 type CoordS = `${number}_${number}_${number}_${number}`;
-type Conway = Set<CoordS>;
+type Conway = Map<CoordS, boolean>;
 
-const parse = (lines: string[]): Conway => new Set([...(function* () {
+const parse = (lines: string[]): Conway => new Map([...(function* () {
     for (let y = 0; y < lines.length; ++y) {
         for (let x = 0; x < lines[0].length; ++x) {
             if (lines[y][x] == '#') {
-                yield toStr([x, y, 0, 0]);
+                yield [toStr([x, y, 0, 0]), true] as const;
             }
         }
     }
@@ -37,27 +37,26 @@ function* neighboors(s: CoordS): Iterable<CoordS> {
     }
 };
 
-const step = (conway: Conway): Conway => {
-    const consider = new Map([...(function* () {
-        for (const coord of conway) {
-            yield [coord, true] as const;
-            yield* map(neighboors(coord), n => [n, conway.has(n)] as const);
-        }
-    })()]);
+function* expand(conway: Conway): Iterable<readonly [CoordS, boolean]> {
+    for (const [coord, active] of conway) {
+        yield [coord, active] as const;
+        yield* map(neighboors(coord), n => [n, conway.get(n) ?? false] as const);
+    }
+}
 
-    return new Set([...(function* () {
-        for (const [c, active] of consider) {
-            const count = countBy(neighboors(c), x => consider.get(x) ?? false);
-            if (count === 3 || (active && count === 2)) {
-                yield c;
-            }
+function* step(conway: Conway): Iterable<readonly [CoordS, boolean]> {
+    for (const [c, active] of conway) {
+        const count = countBy(neighboors(c), x => conway.get(x) ?? false);
+        if (count === 3 || (active && count === 2)) {
+            yield [c, true] as const;
         }
-    })()]);
-};
+    }
+}
 
 const part2 = (conway: Conway): number => {
     for (let i = 0; i < 6; ++i) {
-        conway = step(conway);
+        conway = new Map(expand(conway));
+        conway = new Map(step(conway));
     }
 
     return conway.size;
